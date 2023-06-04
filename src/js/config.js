554b5d6106f752e059993b714caf452ff15d43cf
eng-zubair <zachohan85@gmail.com>
2023-06-04 23:06:17 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
+export const KEY = 'edb7c5f8-744c-4b12-92ae-470905f3e536';
+export const MODAL_CLOSE_SEC = 2.5;
