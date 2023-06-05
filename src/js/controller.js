7800ac15a89c147d5039ed7af8419bf151863d63
eng-zubair <zachohan85@gmail.com>
2023-06-05 21:24:07 +0500

    Edit line

--- src/js/controller.js
+++ src/js/controller.js
@@ -137,6 +137,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-newFeature();
+  newFeature();
 };
 init();
